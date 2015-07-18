ca0b0b031c8e619693f44b61e4fa71b69d37b8cf
yuriybon <yuriy_bon@mail.ru>
2015-07-18 09:44:37 +0300

    Task6

---
+++ BaseJava/src/Task6.java
@@ -0,0 +1,18 @@
+
+public class Task6 {
+	/**
+	 * 6. Дано значение температуры T в градусах Фаренгейта. Определить значение этой же температуры в градусах Цельсия. Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношеним: TC=(TF–32)·5/9.
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tf =  273;
+		double tc = 0;
+		
+		System.out.println((tf-32)*5/9);
+		
+
+	}
+
+}
